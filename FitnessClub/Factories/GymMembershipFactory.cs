8caac6a2ee17cb5fd56b9b8f5b19d663e161230c
Artur Danilov <arthurdanilov1990@gmail.com>
2023-03-07 10:52:03 +0100

    Add project files.

---
+++ FitnessClub/Factories/GymMembershipFactory.cs
@@ -0,0 +1,30 @@
+﻿using FitnessClub.Domain;
+
+namespace FitnessClub.Factories
+{
+    //только походы в зал
+    internal class GymMembershipFactory : MembershipFactory
+    {
+        //цена и описание принимаются через конструктор
+        private readonly decimal _price;
+        private readonly string _description;
+
+        public GymMembershipFactory(decimal price, string description)
+        {
+            _price = price;
+            _description = description;
+        }
+
+        //реализация фабричного метода
+        public override IMembership GetMembership() //обязаны вернуть абонимент реализующий интерфейс IMembership
+        {
+            //создаем абонимент походов в зал
+            GymMembership membership = new(_price)
+            {
+                Description = _description
+            };
+
+            return membership;
+        }
+    }
+}
